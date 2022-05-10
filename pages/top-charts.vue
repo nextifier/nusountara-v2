98a228ae00ce87ce7db450c98b0fb196c7d5bcee
Nextifier <nextifier@gmail.com>
2022-05-10 20:29:06 +0700

    updates

--- pages/top-charts.vue
+++ pages/top-charts.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="py-10 md:py-16">
-    <div class="container mx-auto px-4">
+    <div class="container max-w-md mx-auto px-4">
       <h1
         class="text-white font-bold tracking-tight text-3xl md:text-4xl flex items-baseline gap-x-2"
       >
@@ -8,7 +8,68 @@
         <span class="bg-primary h-2.5 w-5"></span>
       </h1>
 
-      <div class="min-h-[600px] rounded-xl bg-gray-900 w-full mt-8"></div>
+      <div class="genres flex flex-col mt-10">
+        <span class="font-bold text-base">Choose Genre</span>
+        <div v-if="genres && genres.length" class="flex flex-wrap gap-2 mt-3">
+          <button
+            type="button"
+            v-for="(genre, index) in genres"
+            :key="index"
+            class="px-4 py-2 rounded-lg border border-gray-500"
+            :class="{ active: activeGenre == genre.slug }"
+            v-wave
+            @click="activeGenre = genre.slug"
+          >
+            {{ genre.name }}
+          </button>
+        </div>
+      </div>
+
+      <div v-if="genres && genres.length">
+        <div v-for="(genre, index) in genres" :key="index">
+          <div v-if="genre.slug == activeGenre">
+            <div
+              v-if="genre.tracks && genre.tracks.length"
+              class="flex flex-col gap-y-2 md:gap-y-4 mt-10"
+            >
+              <div
+                v-for="(track, index) in genre.tracks"
+                :key="index"
+                class="flex items-center gap-x-3 md:gap-x-5"
+              >
+                <div
+                  class="rank-number w-4 flex-shrink-0 flex-grow-0 font-bold flex flex-col items-center gap-y-1"
+                >
+                  <div v-if="index < 3">
+                    <IconStar
+                      class="h-3"
+                      :class="{
+                        'text-[#FACC15]': index == 0,
+                        'text-[#94A3B8]': index == 1,
+                        'text-[#C47D36]': index == 2,
+                      }"
+                    />
+                  </div>
+                  {{ index + 1 }}
+                </div>
+                <nuxt-link :to="`/tracks/${track.slug}`">
+                  <div
+                    class="w-16 h-16 bg-gray-900 rounded-lg flex-shrink-0 flex-grow-0"
+                  ></div>
+                </nuxt-link>
+                <div class="flex flex-col items-start gap-y-0.5">
+                  <nuxt-link
+                    :to="`/tracks/${track.slug}`"
+                    class="font-bold text-white text-base sm:text-lg line-clamp-1"
+                    >{{ track.title }}</nuxt-link
+                  >
+                  <span class="line-clamp-1">{{ track.artist }}</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -20,7 +81,22 @@ export default {
       title: `Top Charts – ${this.$config.appName}`,
     };
   },
+
+  data() {
+    return {
+      genres: [],
+      activeGenre: "house",
+    };
+  },
+
+  created() {
+    this.genres = this.$store.state.topCharts.genres;
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+.genres .active {
+  @apply bg-primary text-white font-bold border-transparent;
+}
+</style>
--- store/tracks.js
+++ store/tracks.js
@@ -735,6 +735,116 @@ export const state = () => ({
       },
       created_at: new Date(2021, 10, 12),
     },
+    {
+      title: 'Midnight in Peckham',
+      slug: 'midnight-in-peckham',
+      artist: 'Chaos in the CBD',
+      genres: ['House'],
+      cover_image: 'Midnight in Peckham.jpg',
+      spotify_embed_src: 'https://open.spotify.com/embed/track/1CzfZY7lXaysy84x3a2jix?utm_source=generator&theme=0',
+      link: {
+        spotify: 'https://open.spotify.com/track/1CzfZY7lXaysy84x3a2jix?si=a0d8071da3e64cdc',
+      },
+    },
+    {
+      title: 'Set It Out',
+      slug: 'set-it-out',
+      artist: 'Omar S',
+      genres: ['House'],
+      cover_image: 'Set It Out.jpg',
+      spotify_embed_src: 'https://open.spotify.com/embed/track/55uBEgwtpTx5NGgzkoDoAx?utm_source=generator&theme=0',
+      link: {
+        spotify: 'https://open.spotify.com/track/55uBEgwtpTx5NGgzkoDoAx?si=cb86549824ee47d2',
+      },
+    },
+    {
+      title: 'Selfless State',
+      slug: 'selfless-state',
+      artist: 'So Inagawa',
+      genres: ['House'],
+      cover_image: 'Selfless State.jpg',
+      spotify_embed_src: 'https://open.spotify.com/embed/playlist/1oc323dm0734ZasBpqQGou?utm_source=generator&theme=0',
+      link: {
+        spotify: 'https://open.spotify.com/playlist/1oc323dm0734ZasBpqQGou?si=09892a3dbefb4a68',
+      },
+    },
+    {
+      title: 'Footwork',
+      slug: 'footwork',
+      artist: 'Theo Parrish',
+      genres: ['House'],
+      cover_image: 'Footwork.jpg',
+      spotify_embed_src: 'https://open.spotify.com/embed/track/2m7DFd2aOXVpNlA5KeAlA7?utm_source=generator&theme=0',
+      link: {
+        spotify: 'https://open.spotify.com/track/2m7DFd2aOXVpNlA5KeAlA7?si=5459776b49324d31',
+      },
+    },
+    {
+      title: '3D Print',
+      slug: '3d-print',
+      artist: 'DJ Sneak',
+      genres: ['House'],
+      cover_image: '3D Print.jpg',
+      spotify_embed_src: 'https://open.spotify.com/embed/track/5gp6c8VfyXDQLwu9arBvSe?utm_source=generator&theme=0',
+      link: {
+        spotify: 'https://open.spotify.com/track/5gp6c8VfyXDQLwu9arBvSe?si=785325fed9194ac3',
+      },
+    },
+    {
+      title: 'It\'s Over Love',
+      slug: 'its-over-love',
+      artist: 'Todd Terry',
+      genres: ['House'],
+      cover_image: 'It\'s Over Love.jpg',
+      spotify_embed_src: 'https://open.spotify.com/embed/track/3t2DaDCFAj6ko9RFQxvyaA?utm_source=generator&theme=0',
+      link: {
+        spotify: 'https://open.spotify.com/track/3t2DaDCFAj6ko9RFQxvyaA?si=794666bb402a4c5a',
+      },
+    },
+    {
+      title: 'The Way You Like',
+      slug: 'the-way-you-like',
+      artist: 'Gene Farris',
+      genres: ['House'],
+      cover_image: 'The Way You Like.jpg',
+      spotify_embed_src: 'https://open.spotify.com/embed/track/0Sw5puLByGkHnj4EQGjIcx?utm_source=generator&theme=0',
+      link: {
+        spotify: 'https://open.spotify.com/track/0Sw5puLByGkHnj4EQGjIcx?si=2b3713806ee94788',
+      },
+    },
+    {
+      title: 'Who\'s Afraid of Detroit',
+      slug: 'who-is-afraid-of-detroit',
+      artist: 'Claude VonStroke',
+      genres: ['House'],
+      cover_image: 'Who\'s Afraid of Detroit.jpg',
+      spotify_embed_src: 'https://open.spotify.com/embed/track/1KiNEbmfe20LO0jitOp8Ao?utm_source=generator&theme=0',
+      link: {
+        spotify: 'https://open.spotify.com/track/1KiNEbmfe20LO0jitOp8Ao?si=7ef4357d76eb4246',
+      },
+    },
+    {
+      title: 'Get on Down',
+      slug: 'get-on-down',
+      artist: 'Kenny Dope',
+      genres: ['House'],
+      cover_image: 'Get on Down.jpg',
+      spotify_embed_src: 'https://open.spotify.com/embed/track/4PEdxcK7pPL6TWM0lUSh5K?utm_source=generator&theme=0',
+      link: {
+        spotify: 'https://open.spotify.com/track/4PEdxcK7pPL6TWM0lUSh5K?si=89d6177fc3c24d3c',
+      },
+    },
+    {
+      title: 'Believe',
+      slug: 'believe',
+      artist: 'Fred Everything',
+      genres: ['House'],
+      cover_image: 'Believe.jpg',
+      spotify_embed_src: 'https://open.spotify.com/embed/track/14WqqgfrkNvD9ll7DBUsvl?utm_source=generator&theme=0',
+      link: {
+        spotify: 'https://open.spotify.com/track/14WqqgfrkNvD9ll7DBUsvl?si=df6b698312cc483f',
+      },
+    },
   ],
 })
 
