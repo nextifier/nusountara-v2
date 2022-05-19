5867e4e56587eb524b104647016651ddd666fc14
Nextifier <nextifier@gmail.com>
2022-05-19 16:23:31 +0700

    updates

--- store/artists.js
+++ store/artists.js
@@ -20,7 +20,16 @@ export const state = () => ({
           <p>After being introduced to acts like the Chemical Brothers and Underworld at a very young age, her infatuation with music blossomed into a virus when she went to her first ever rave in San Francisco where she moved from Japan to study. Not bound to any particular subgenre, her seamless blend of trance, techno and progressive house has lit up dance floors across the world and her ever evolving productions always follow the trend in riveting melodic fashion.</p>
           <p>Having achieved significant global successes already, Apsara isn't resting on her laurels. She's plotting world tours, her own events brand, international residencies and many more releases full of pulsating basslines and ever evolving melodies.</p>
           <p>Two stars are now rising in the east and they will both light up your world. But only Apsara will do it permanently. Wait and see.</p>
-        `
+      `,
+      discography: [
+        'Impala (Truesounds Music)',
+        'Jura (Truesounds Music)',
+        'Bayle (Truesounds Music)',
+        'The Cell (Percomaniacs)',
+        'Simply Red (Monaberry)',
+        'Power House (Awen Records)',
+        'Filtering History (Awen Records)',
+      ]
     },
     {
       id: 'A002',
@@ -34,42 +43,31 @@ export const state = () => ({
       soundcloud: 'https://m.soundcloud.com/cyda_music',
       mixcloud: 'https://m.mixcloud.com/cydamusic/',
       biography: `
-          <p>Cyda was born in April 17th, 1990 in Bandung. This man has musician gene from his mother who works as Indonesian traditional singer (sinden). He began his musical career since he was in senior high school in 2007. He established a band specializing in chiptune genre. Thenceforward, he and his musican genre like a solid soulmate.</p>
-          <p>No wonder, he also a musician, arranger, producer, and DJ. He then becomes a music producer until this time. Now his passion music in dark disco, italo disco, synthwave, and house music. Some of the awards he got in the Indonesian electronic dance music industry are "PRODUCER OF THE YEAR PARANOIA AWARDS 2017" and "HOUSE DJ OF THE YEAR PARANOIA AWARDS 2018".</p>
-          <p>He has released many songs and signed "Space Energy EP" with Soulfooled records berlin "Alex Niggemann's label". Cyda is also working as a wMusic Director. Until now he is still the unstoppable talent that would make his move hypnotize the crowd.</p>
-          <span>GIGS EXPERIENCE:</span>
+          <p>Cyda was born in April 17th, 1990 in Bandung. This mas has musician gene from his mother who work as Indonesian traditional singer (Sinden). He began his musical career since he wa in senior high school in 2007. He established a band specializing in chiptune genre. Thenceforward, he and his music genre were like a solid soulmate.</p>
+          <p>No wonder, he also become Musician, Arranger, Producer and DJ. Now his passion music in Dark Disco, Italo Disco, Synthwave and House music. Some of award he got in  the Indonesian electronic dance music industry are: Producer Of The Year Paranoia Award 2017 and House DJ Of The Year Paranoia Award 2018.</p>
+          <p>He has released many songs and signed “Space Energy EP” to Alex Niggeman’s label Soulfooled Records imprint in Berlin. Cyda is also working as Music Director. Until now he still unstoppable talent that would make his move to hypnotize the crowd.</p>
+          <p>GIGS EXPERIENCE:</p>
+          <span>Asia Tour</span>
           <ul>
-            <li>JAPAN TOUR: Dimension Tokyo, Substore Tokyo, Joujou Nishiazabu</li>
-            <li>ASIA TOUR: Artemis Singapore, FONO Malaysia, UNDERGROUND Nihonbashi Japan, Bridge Shibuya Japan, EN-SOF Shibuya Japan</li>
+            <li>EN-SOF SHIBUYA – JAPAN</li>
+            <li>DIMENSION SHIBUYA – JAPAN</li>
+            <li>UNDERGROUND NIHONBASHI – JAPAN</li>
+            <li>COOL DREAD ASAGAYA – JAPAN</li>
+            <li>MEDIUM KIYOSUMISHIRAKAWA  - JAPAN</li>
+            <li>SUBSTORE KOENJI – JAPAN</li>
+            <li>ARTEMIS – SINGAPORE</li>
+            <li>FONO KUALA LUMPUR – MALAYSIA</li>
           </ul>
       `,
       discography: [
-        'Dark Energy',
-        'Space In Your Eye',
-        'Hoag\'s Object',
-        'Speak Out',
-        'Stellar',
-        'Move Your Hands',
-        'Daytime Soul',
-        'Feel The Melody',
-        'Get Down to the Music',
-        'Body Move',
-        'Sambalado',
-        'So What To See',
-        'Midnight In Palma',
-        'This Is The Time',
-        'Feeling',
-        'Squid',
-        'It\'s Fabulous',
-        'Alright',
-        'Let Me Tell You',
-        'Can You Feel It',
-        'Orchid',
-        'In The Night',
-        'Funky Jack',
-        'Atmos',
-        'Casino Theme',
-        'Salt',
+        'SPACE ENERGY EP (SOULFOOLED RECORDS)',
+        'STELLAR (LOVE INTERNATIONAL)',
+        'DAYTIME SOUL (CLUB SESSION)',
+        'MOVE YOUR HANDS EP (POGO HOUSE RECORDS',
+        'IT’S FABULOUS EP (ONAKO RECORDS)',
+        'FUNKY JACK “ADE COMPILATION 2018” (UNION RECORDS)',
+        'FEELING EP (POGO HOUSE RECORDS)',
+        'ATMOS EP (SOUL SHIFT MUSIC)',
       ]
     },
     {
@@ -84,7 +82,7 @@ export const state = () => ({
       soundcloud: 'https://soundcloud.com/nandi-wardana',
       mixcloud: 'https://www.mixcloud.com/anandiwp/',
       biography: `
-          <p>Nandi is an Indonesian DJ who has deep passion in electronic music and that's what he always trying to present everytime he played, music with passi</p>
+          <p>Nandi is an Indonesian DJ who has deep passion in electronic music and that's what he always trying to present everytime he played, music with passion</p>
           <p>Either playing for small bars or big clubs, his aim is to deliver the right music for the right audience and creating the perfet vibe and journey. Gig wise he has been playing around regularly at some of Jakarta's best nightclub such as Jenja Jakarta, Club Colosseum as well as countless bars and lounges, while making his first international debut at Club Kyo Singapore in 2016.</p>
           <p>Nandi is also part of House Cartel collective that consist of like minded individuals behind it with an aim to pushing local House and Techno music scene in Indonesia. During 5 years of its existence, House Cartel has already making big impact in Jakarta and South East Asia music scene by creating lots of memorable parties and promoting emerging talents in the region.</p>
         `
